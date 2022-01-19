5b0f724eb33e987658e32f211f1c12a11d039df9
rassi0429 <taitokokoa+rassi@gmail.com>
2022-01-19 17:24:04 +0900

    feat: v1.0.0

---
+++ SaveToWhere/SaveToWhere.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Reflection;
+using FrooxEngine;
+using BaseX;
+using CloudX.Shared;
+using CodeX;
+using FrooxEngine.UIX;
+using System.Collections.Generic;
+
+namespace SaveToWhere
+{
+    public class SaveToWhere : NeosMod
+    {
+        public override string Name => "SaveToWhere";
+        public override string Author => "kka429";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/rassi0429/SaveToWhere"; // this line is optional and can be omitted
+
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("dev.kokoa.savetowhere");
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(LocaleHelper), "AsLocaleKey", new Type[] { typeof(string), typeof(bool), typeof(Dictionary<string, object>) })]
+        class Patch
+        {
+            static void Postfix(ref LocaleString __result, string str)
+            {
+                if(str == "Interaction.SaveToInventory")
+                {                 
+                    __result = "Save To : \n /" + InventoryBrowser.CurrentUserspaceInventory?.CurrentPath;
+                }
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# MessageCopy
+
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/)  
+The text message will be copied to the clipboard by the trigger
+
+I didn't create an issue because of a limited issue for me, but I did.
+https://github.com/Neos-Metaverse/NeosPublic/issues/3401
+
+## Installation
+1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
+1. Place [MessageCopy.dll](https://github.com/rassi0429/MessageCopy/releases/latest/download/MessageCopy.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
